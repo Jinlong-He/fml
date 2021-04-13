bc193a8018c5b9aace3e4b9827096b4dbb29ddb3
hejl <loringhe@outlook.com>
2021-04-13 17:55:14 +0800

    feat: add shuffle in util

--- include/util/util.hpp
+++ include/util/util.hpp
@@ -12,8 +12,9 @@
 #include <iostream> 
 #include <unordered_set>
 #include <vector>
-using std::vector;
-using std::string;
+using std::vector, std::string;
+using std::cout;
+using std::endl;
 
 namespace util {
     template <class Set>
@@ -83,6 +84,51 @@ namespace util {
             p = strtok(NULL, d);  
         }  
         return res;  
+    }
+
+    template <class T>
+    static inline void
+    shuffle(const vector<T>& datas1, const vector<T>& datas2, 
+            const vector<vector<T> >& datas, vector<vector<T> >& res) {
+        if (datas1.size() == 0 && datas2.size() == 0) {
+            res = datas;
+            return;
+        }
+        vector<vector<T> > new_res1, new_res2;
+        if (datas1.size() > 0) {
+            vector<vector<T> > new_datas;
+            for (auto& data : datas) {
+                vector<T> new_data = data;
+                new_data.emplace_back(datas1[0]);
+                new_datas.emplace_back(new_data);
+            }
+            if (datas.size() == 0) {
+                new_datas.emplace_back(vector<T>({datas1[0]}));
+            }
+            vector<T> new_datas1(datas1.begin() + 1, datas1.end());
+            shuffle(new_datas1, datas2, new_datas, new_res1);
+        }
+        if (datas2.size() > 0) {
+            vector<vector<T> > new_datas;
+            for (auto& data : datas) {
+                vector<T> new_data = data;
+                new_data.emplace_back(datas2[0]);
+                new_datas.emplace_back(new_data);
+            }
+            if (datas.size() == 0) {
+                new_datas.emplace_back(vector<T>({datas2[0]}));
+            }
+            vector<T> new_datas2(datas2.begin() + 1, datas2.end());
+            shuffle(datas1, new_datas2, new_datas, new_res2);
+        }
+        res = new_res1;
+        res.insert(res.end(), new_res2.begin(), new_res2.end());
+    }
+
+    template <class T>
+    static inline void
+    shuffle(const vector<T>& datas1, const vector<T>& datas2, vector<vector<T> >& res) {
+        shuffle(datas1, datas2, vector<vector<T> >(), res);
     }
 }
 
